86e697ec46dddc520724db086870440c5562084a
Matteo Casarin <casarinmatteo22@gmail.com>
2018-03-20 14:50:56 +0100

    added /projects route

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Projects portfolio'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Error handling request'
